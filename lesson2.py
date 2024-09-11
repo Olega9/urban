f49fee347e5c98d8597b08003bd9ad3f7acb7d03
Olega9 <muffler.boron_0m@icloud.com>
2024-09-11 17:01:48 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+NumberOfCompletedHomework='12'
+NumberOfHoursSpent='1.5'
+CourseName='Python'
+TimeForOneTask=(1.5/12)
+print(CourseName,'всего задач'':',NumberOfCompletedHomework,'затрачено часов'':',NumberOfHoursSpent,'среднее время выполнения',TimeForOneTask)
