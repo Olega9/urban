6384fc9ff9ed62f279e3e7329c96b9f19f43e2d7
Olega9 <muffler.boron_0m@icloud.com>
2024-09-14 13:38:02 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,17 @@
+my_dict={"номер телефона":5559955, "номер договора":9595500,"адрес":'арбат'}
+print(my_dict)
+print(my_dict['адрес'])
+my_dict['город']='москва'
+print(my_dict['город'])
+my_dict.update({'max':98484,'john': 94400})
+print(my_dict)
+a=my_dict.pop('город'and'адрес')
+print(a)
+print(my_dict)
+my_set={1,2,7,9,9,0,2,2,3}
+print(my_set.add(6))
+print(my_set.add(4))
+print(my_set.discard(9))
+print(my_set)
+
+
