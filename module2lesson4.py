8570cf4590239f4574d3544fa23ad6a26dbd6d18
Olega9 <muffler.boron_0m@icloud.com>
2024-10-24 14:40:33 +0300

    Add files via upload

--- module2lesson4.py
+++ module2lesson4.py
@@ -1,19 +1,23 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
 primes = []
 not_primes = []
 
-for key in numbers:
+for number in numbers:
+    if number == 1:
+        continue
+
     is_prime = True
-    if key < 2:
-        is_prime = False
-    else:
-        for i in range(2, key):
-            if key % i == 0:
-                is_prime = False
-                break
+
+    for divisor in range(2, number):
+        if number % divisor == 0:
+            is_prime = False
+            break
+
     if is_prime:
-        primes.append(key)
+        primes.append(number)
     else:
-        not_primes.append(key)
+        not_primes.append(number)
+
 print("Простые числа:", primes)
-print("Не простые числа:", not_primes)
+print("Непростые числа:", not_primes)
