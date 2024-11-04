2e52560f8dd07e29b829324a90195f32c1eef6d6
Olega9 <muffler.boron_0m@icloud.com>
2024-11-04 23:50:35 +0300

    Add files via upload

--- 3,5.py
+++ 3,5.py
@@ -3,7 +3,8 @@ def get_multiplied_digits(number):
     if len(str_number) == 1:
         return int(str_number)
     first = int(str_number[0])
+    if first == 0:
+        return get_multiplied_digits(int(str_number[1:]))
     return first * get_multiplied_digits(int(str_number[1:]))
-
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(420)
 print(result)
