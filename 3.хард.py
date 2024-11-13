cdd7ec8704d1c64085aa9fb72862e2e131807adb
Olega9 <muffler.boron_0m@icloud.com>
2024-11-13 11:29:01 +0300

    Add files via upload

--- 3.хард.py
+++ 3.хард.py
@@ -1,29 +1,17 @@
 def calculate_structure_sum(data):
-    total_sum = 0
-
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(key, str):
-                total_sum += len(key)
-            total_sum += calculate_structure_sum(value)
-
-    elif isinstance(data, (list, tuple)):
-        for item in data:
-            total_sum += calculate_structure_sum(item)
-
+    total = 0
+    if isinstance(data, int):
+        total += data
     elif isinstance(data, str):
-        total_sum += len(data)
+        total += len(data)
 
-    elif isinstance(data, (int, float)):
-        total_sum += data
+    elif isinstance(data, (list, tuple, set)):
+        for item in data:
+            total += calculate_structure_sum(item)
 
-    return total_sum
-data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])]
+    elif isinstance(data, dict):
+        for key, value in data.items():
+            total += calculate_structure_sum(key)
+            total += calculate_structure_sum(value)
 
-result = calculate_structure_sum(data_structure)
-print(result) 
+    return total
