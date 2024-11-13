456f9521857b3b34d48b40f5d2007577550c0744
Olega9 <muffler.boron_0m@icloud.com>
2024-11-13 11:50:17 +0300

    Add files via upload

--- 4.1.py
+++ 4.1.py
@@ -1,10 +1,10 @@
-from fake_math import divide as fake_divide
-from true_math import divide as true_divide
+from fake_math import divide as fak
+from true_math import divide as true
 
-result1 = fake_divide(95, 6)
-result2 = fake_divide(8, 0)
-result3 = true_divide(89, 4)
-result4 = true_divide(77, 0)
+result1 = fak(95, 6)
+result2 = fak(8, 0)
+result3 = true(89, 4)
+result4 = true(77, 0)
 
 print(result1)
 print(result2)
